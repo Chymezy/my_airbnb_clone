1e22fdf57e555b56dc253be4f5713abe162278bd
Joshua Benjamin <chymezy332@gmail.com>
2024-04-29 17:06:05 -0700

    adequate unit testing

--- tests/test_model/test_base_model.py
+++ tests/test_model/test_base_model.py
@@ -1,49 +1,76 @@
 import unittest
-from models import BaseModel
+from datetime import datetime
+from models.base_model import BaseModel
 
-""" 
-baseModel unit testing file
-"""
-class Test_BaseModel(unittest.TestCase):
-    """"
-    1. test if class is as basemodel
-    2. test if id attr. is an istance of BaseModel
-    3. test id is of type str
-    4. check if created_at attr. is same as updated_at
-    5  check if created_at and updated_at is of iso-time format
-
-    1. test if __str__() returns required format.
-
-    1. test if update_at attr changed when save method is called
-    
-    1. test if self.__dict__ is same as value of obj attr*
-    2. test if __class__ is same as __dict__ key.
-    3. test if created_at and update_at aligns to iso time format
+class TestBaseModel(unittest.TestCase):
     """
-        
-    obj = BaseModel()
+    Test cases for the BaseModel class.
+    """
 
-    def test_BaseModel_Class(self):
-        ''' test if class is basemodel '''
-        self.assertIsInstance(obj, BaseModel)
+    def test_instance_of_base_model(self):
+        """
+        Test if the class is an instance of BaseModel.
+        """
+        model = BaseModel()
+        self.assertIsInstance(model, BaseModel)
 
-    def test_obj_id(self):
-        ''' test if id attr is a field of BaseModel instance '''
-        self.assertIsInstance(obj.id, BaseModel)
+    def test_id_is_string(self):
+        """
+        Test if the id attribute is an instance of str.
+        """
+        model = BaseModel()
+        self.assertIsInstance(model.id, str)
 
-    def test_objId_str(self):
-        ''' test obj id if it is str '''
-        self.assertIsInstance(obj.id, str)
+    def test_id_is_string(self):
+        """
+        Test if the id attribute is of type str.
+        """
+        model = BaseModel()
+        self.assertIsInstance(model.id, str)
 
-    def test_obj_created_at_updated_at(self):
-        ''' test obj created_at and updated_at if same '''
-        self.assertEqual(obj.created_at, obj.updated_at)
+    def test_created_at_equals_updated_at(self):
+        """
+        Test if created_at equals updated_at.
+        """
+        model = BaseModel()
+        self.assertEqual(model.created_at, model.updated_at)
 
-    def test_isoformat(self):
-        ''' test created_at attr time format '''
-        self.assertEqual(obj.created_at, '%Y-%m-%dT%H:%M:%S.%f')
-        self.assertEqual(obj.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+    def test_created_at_iso_format(self):
+        """
+        Test if created_at and updated_at are in ISO format.
+        """
+        model = BaseModel()
+        self.assertTrue(datetime.fromisoformat(model.created_at.isoformat()))
+        self.assertTrue(datetime.fromisoformat(model.updated_at.isoformat()))
 
-    def test_str(self):
-        ''' test str output '''
-        self.assertEqual(str(obj),"[self.__class__.__name__] (self.id) self.__dict__ ")
+    def test_str_method(self):
+        """
+        Test if __str__() returns the required format.
+        """
+        model = BaseModel()
+        expected_str = "[BaseModel] ({}) {}".format(model.id, model.__dict__)
+        self.assertEqual(str(model), expected_str)
+
+    def test_updated_at_changes_on_save(self):
+        """
+        Test if updated_at changes when save() method is called.
+        """
+        model = BaseModel()
+        old_updated_at = model.updated_at
+        model.save()
+        new_updated_at = model.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+
+    def test_to_dict_method(self):
+        """
+        Test if to_dict() method returns expected values.
+        """
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['created_at'], model.created_at.isoformat())
+        self.assertEqual(model_dict['updated_at'], model.updated_at.isoformat())
+
+if __name__ == '__main__':
+    unittest.main()
+
