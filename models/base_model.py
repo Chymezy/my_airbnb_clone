4aa895c9cb67b224273a416e4f134bc20579a079
Joshua Benjamin <chymezy332@gmail.com>
2024-04-29 17:15:52 -0700

    adequate unit testing

--- models/base_model.py
+++ models/base_model.py
@@ -6,14 +6,15 @@ from datetime import datetime
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        current_time = datetime.utcnow()
+        self.created_at = current_time
+        self.updated_at = current_time
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
