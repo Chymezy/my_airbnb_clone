f78b3025c7ec9fb5a4addd4127f9e11f8a8fdbcb
Chymezy <chymezy332@gmail.com>
2024-04-30 14:49:13 +0100

    updating model __init__() method for object recreation from keyworded arguments

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,19 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         current_time = datetime.utcnow()
         self.created_at = current_time
         self.updated_at = current_time
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value) 
+
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -24,14 +32,14 @@ class BaseModel:
         return obj_dict
 
 if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
+    my_model = BaseModel(name='Benjamin Model', my_number='87')
+    # my_model.name = "My First Model"
+    # my_model.my_number = 89
+    # print(my_model)
     my_model.save()
-    print(my_model)
+    # print(my_model)
     my_model_json = my_model.to_dict()
-    print(my_model_json)
+    # print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
         print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
