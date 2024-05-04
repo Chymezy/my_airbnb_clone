8f91b749723a1b0a3913c922d351b0f10746f5a2
Chymezy <chymezy332@gmail.com>
2024-05-04 17:44:25 +0100

    corrected __str__() formatting

--- try/models/base_model.py
+++ try/models/base_model.py
@@ -23,7 +23,7 @@ class BaseModel:
     
     def __str__(self):
         ''' formats instance output '''
-        return f'[{self.__class__.__name__}], ({self.id}), {self.__dict__}'
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
         ''' saves creation and modification of instances '''
