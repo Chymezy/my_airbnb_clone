e16b454f569270794d30b029e629426b00429ed7
Chymezy <chymezy332@gmail.com>
2024-05-05 10:54:45 +0100

    console show() implemented

--- try/console.py
+++ try/console.py
@@ -2,6 +2,7 @@
 
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
 ''' This the console module '''
 
@@ -52,24 +53,40 @@ class HBNBCommand(cmd.Cmd):
         if not line:
             print("** class name missing **")
             return
-        
-        approved_classes = ['BaseModel', 'User']
-        args = line.split()
-        class_name = args[0]
-        class_id = args[1]
-        class_types = []
-        if class_name in approved_classes:
-            class_type = globals().get(class_name)
-            if class_type is not None:
-                class_types.append(class_type)
-                return
         else:
-            print("** class doesn't exist **")
-        if class_id:
-            pass
 
+            approved_classes = ['BaseModel', 'User']
+            args = line.split()
+            ''' check class '''
+            if len(args) > 0: #True
+                class_name = args[0] # BaseModel
+                class_types = [] #'<model.base_model.BaseModel>'
+                if class_name in approved_classes:
+                    class_type = globals().get(class_name) #'<model.base_model.BaseModel>'
+                    if class_type is not None:
+                        class_types.append(class_type)
 
-
+                    ''' check id '''    
+                    if len(args) > 1:    
+                        class_id = args[1] 
+                        if class_id:
+                            all_objs = storage.all()
+                            ids = []
+                            for obj_id in all_objs.keys():
+                                ids.append(obj_id.split('.')[1])
+                            if class_id in ids:
+                                key = f'{class_name}.{class_id}'
+                                print(all_objs[key])
+                                return
+                            else:
+                                print("** no instance found **")
+                                return
+                    else:
+                        print('** instance id missing **')
+                        return
+            else:
+                print("** class doesn't exist **")
+                return
 
 
 
--- try/test_reload.py
+++ try/test_reload.py
@@ -3,14 +3,20 @@ from models import storage
 from models.base_model import BaseModel
 
 all_objs = storage.all()
+checks = '7c57982e-9312-40d4-b9c9-99731cd140d2'
+ids = []
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
+    ids.append(obj_id.split('.')[1])
+    # obj = all_objs[obj_id]
+    # print(obj
+if checks in ids:
+    print("I found it")
+# print(ids)
 
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
+# print("-- Create a new object --")
+# my_model = BaseModel()
+# my_model.name = "My_First_Model"
+# my_model.my_number = 89
+# my_model.save()
+# print(my_model)
