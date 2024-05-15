011e3dfd2b637e5286309158004ccc992910b5b6
Chymezy <chymezy332@gmail.com>
2024-05-15 10:26:09 +0100

    dealing with orm

--- my_airbnb_mySQL/sqlAchemy/connect.py
+++ my_airbnb_mySQL/sqlAchemy/connect.py
@@ -1,14 +1,1 @@
-from sqlalchemy import create_engine, text
-
-# create engine
-engine = create_engine("mysql+mysqlconnector://chymezy:password@localhost:3306/josh_db")
-
-with engine.connect() as connection:
-    # Create a text construct for the SQL query
-    query = text('select "Hello"')
-    
-    # Execute the query
-    result = connection.execute(query)
-    
-    # Fetch and print the results
-    print(result.fetchall())
+from sqlalchemy import create_engine
---
+++ my_airbnb_mySQL/sqlAchemy/main.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+''' create engine to your database '''
+engine = create_engine('mysql+mysqlconnector://chymezy:password@localhost/josh_db')
+
+''' create a connection or traction handler with db '''
+Session = sessionmaker(bind=engine)
+session = Session()
+
+''' handles your model definitions and packaging '''
+Base = declarative_base()
+
+''' model your table '''
+class Student(Base):
+    __tablename__ = 'students'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+
+''' create table in database. metadata collates ur table structure '''
+Base.metadata.create_all(engine)
+
+''' insert data into the created table '''
+new_student = Student(name='Joshua Benjamin')
+session.add(new_student)
+session.commit()
+
+''' retrieved data '''
+students = session.query(Student).all()
+for student in students:
+    print(student.id, student.name)
+    
+
+
